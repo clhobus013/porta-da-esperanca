39d73a0a94f15a82efb6d219f5b24937fe624a38
claudia <clauu013@gmail.com>
2022-12-23 19:02:06 -0300

    Adição porta

---
+++ components/Porta.tsx
@@ -0,0 +1,19 @@
+import styles from "../styles/Porta.module.css"
+
+export default function Porta(props:{selecionada:boolean}) {
+
+    const selecionada = props.selecionada ? styles.selecionada : ''
+    return (
+        <div className={styles.area}>
+            <div className={`${styles.moldura} ${selecionada}`}>
+                <div className={styles.porta}>
+                    <div className={styles.numero}>3</div>
+                    <div className={styles.macaneta}></div>
+                </div>
+            </div>
+            <div className={styles.chao}>
+
+            </div>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import Porta from "../components/Porta";
 import Presente from "../components/Presente";
 
 export default function Home() {
   return (
-    <div>
-      <Presente/>
+    <div style={{display: "flex"}}>
+      <Porta selecionada/>
+      <Porta selecionada={false}/>
     </div>
   )
 }
