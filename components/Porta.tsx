50700ee5faf97eb6b64bbbad0ae91ca7e5fb241d
claudia <clauu013@gmail.com>
2022-12-26 14:03:20 -0300

    Aplicando eventos na porta

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -1,16 +1,37 @@
+import PortaModel from "../model/Porta"
 import styles from "../styles/Porta.module.css"
 
-export default function Porta(props:{selecionada:boolean}) {
+interface PortaProps {
+    value: PortaModel
+    onChange: (novaPorta: PortaModel) => void
+}
 
-    const selecionada = props.selecionada ? styles.selecionada : ''
-    return (
-        <div className={styles.area}>
-            <div className={`${styles.moldura} ${selecionada}`}>
-                <div className={styles.porta}>
-                    <div className={styles.numero}>3</div>
-                    <div className={styles.macaneta}></div>
-                </div>
+export default function Porta(props:PortaProps) {
+    const porta = props.value
+
+    const selecionada = porta.selecionada && !porta.aberta ? styles.selecionada : ''
+
+    const alternarSelecao = (e:any) => props.onChange(porta.alternarSelecao());
+    const abrir = (e:any) => {
+        e.stopPropagation();
+        props.onChange(porta.abrir());
+    }
+
+    function renderizarPorta() {
+        return(
+            <div className={styles.porta}>
+                <div className={styles.numero}>{porta.numero}</div>
+                <div className={styles.macaneta} onClick={abrir}></div>
             </div>
+        )
+    }
+    
+    return (
+        <div className={styles.area} onClick={alternarSelecao}>
+            <div className={`${styles.moldura} ${selecionada}`}>
+                { porta.aberta ? false : renderizarPorta() }
+            </div>
+            
             <div className={styles.chao}>
 
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import Porta from "../components/Porta";
 import Presente from "../components/Presente";
+import PortaModel from "../model/Porta";
 
 export default function Home() {
+
+  const [p1, setP1] = useState(new PortaModel(1));
+
   return (
     <div style={{display: "flex"}}>
-      <Porta selecionada/>
-      <Porta selecionada={false}/>
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
     </div>
   )
 }
